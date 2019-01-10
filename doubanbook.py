108f1b2e6406613a3386b273a89a63ff565e0ad1
luckytown <33169010+2374060397@users.noreply.github.com>
2019-01-10 10:10:12 +0800

    Add files via upload

---
+++ doubanbook.py
@@ -0,0 +1,10 @@
+import requests
+import re
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+    }
+url = 'https://book.douban.com/'
+content = requests.get(url,headers=headers).text
+pattern = re.compile('<li.*?cover.*?href="(.*?)".*?title="(.*?)".*?more-meta.*?author">(.*?)</span>.*?year">(.*?)</span>.*?</li>',re.S)
+results = re.findall(pattern,content)
+print(content)
